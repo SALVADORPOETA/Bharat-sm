f54f9930eaf4d40527109a162d84b0c6d46518e1
unknown <dsml@live.com>
2024-09-28 19:45:11 -0600

    Suspense1

--- app/history/page.jsx
+++ app/history/page.jsx
@@ -1,17 +1,19 @@
 import Footer from '@/components/Footer'
 import Navbar from '@/components/Navbar'
 import Subscribe from '@/components/Subscribe'
-import React from 'react'
+import React, { Suspense } from 'react'
 import HistoryContainer from './HistoryContainer'
 
 const History = () => {
   return (
-    <div className="font-tiro">
-      <Navbar />
-      <HistoryContainer />
-      <Subscribe />
-      <Footer />
-    </div>
+    <Suspense>
+      <div className="font-tiro">
+        <Navbar />
+        <HistoryContainer />
+        <Subscribe />
+        <Footer />
+      </div>
+    </Suspense>
   )
 }
 
