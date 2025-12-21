7af5972e1553a5ebf6e56036fbf016d8c84af4b3
Salvador Poeta <dsmlhidroponia@gmail.com>
2025-12-21 03:42:19 -0600

    fix section size

--- app/history/HistoryText.jsx
+++ app/history/HistoryText.jsx
@@ -2,7 +2,7 @@ const HistoryText = ({ section, p1, p2 }) => {
   return (
     <div className="justify-center items-center px-4 pb-8 md:mb-0 lg:mb-12 md:py-2">
       <>
-        <p className="mt-8 mb-6 font-bold text-4xl md:hidden lg:flex">
+        <p className="mt-8 mb-6 font-bold text-2xl md:hidden lg:flex lg:text-4xl">
           {section}
         </p>
         <p className="mt-0">{p1}</p>
